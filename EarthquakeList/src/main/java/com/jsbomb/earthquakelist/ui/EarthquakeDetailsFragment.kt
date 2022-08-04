0c60becb8156d9b8c3ebde29cea1407fa790047e
John Seo <seojohann@gmail.com>
2022-08-04 13:42:49 -0700

    mapview added and applied to EarthquakeDetailsFragment, and removed click listeners on place and coordinate text views.

--- EarthquakeList/src/main/java/com/jsbomb/earthquakelist/ui/EarthquakeDetailsFragment.kt
+++ EarthquakeList/src/main/java/com/jsbomb/earthquakelist/ui/EarthquakeDetailsFragment.kt
@@ -1,22 +1,28 @@
 package com.jsbomb.earthquakelist.ui
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.jsbomb.earthquakelist.databinding.FragmentEarthquakeDetailsBinding
 
 
-class EarthquakeDetailsFragment : Fragment() {
+class EarthquakeDetailsFragment : Fragment(), OnMapReadyCallback {
 
     private lateinit var binding: FragmentEarthquakeDetailsBinding
     private val viewModel: EarthquakeDetailsViewModel by viewModels {
         EarthquakeDetailsViewModelFactory()
     }
+    private var map: GoogleMap? = null
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,11 +31,38 @@ class EarthquakeDetailsFragment : Fragment() {
     ): View {
         binding = FragmentEarthquakeDetailsBinding.inflate(inflater, container, false)
 
+        binding.mapView?.onCreate(savedInstanceState)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewModel.earthquakeDetails.observe(viewLifecycleOwner) { details ->
+            details?.let {
+                binding.details = it
+                val marker = LatLng(it.latitude, it.longitude)
+                map?.let { googleMap ->
+                    googleMap.addMarker(
+                        MarkerOptions()
+                            .position(marker)
+                            .title(it.place)
+                    )
+
+                    googleMap.moveCamera(CameraUpdateFactory.newLatLng(marker))
+                    googleMap.moveCamera(CameraUpdateFactory.zoomTo(9f))
+                }
+            }
+        }
+
+        binding.context = requireContext()
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        binding.mapView?.getMapAsync(this)
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        this.map = map
 
         requireArguments().let { arguments ->
             val time = arguments.getLong(ARG_ITEM_TIME)
@@ -48,30 +81,49 @@ class EarthquakeDetailsFragment : Fragment() {
                 tsunami
             )
         }
+    }
 
-        viewModel.earthquakeDetails.observe(viewLifecycleOwner) { details ->
-            details?.let {
-                binding.details = it
-            }
-        }
+    override fun onStart() {
+        super.onStart()
 
-        viewModel.queryString.observe(viewLifecycleOwner) { uriString ->
-            uriString?.let {
-                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(it)))
-                viewModel.onQueryLocationStart()
-            }
-        }
+        binding.mapView?.onStart()
+    }
 
-        binding.context = requireContext()
-        binding.lifecycleOwner = viewLifecycleOwner
+    override fun onResume() {
+        super.onResume()
 
-        binding.placeView.setOnClickListener {
-            viewModel.onQueryLocation()
-        }
+        binding.mapView?.onResume()
+    }
 
-        binding.coordinatesView.setOnClickListener {
-            viewModel.onQueryLocation()
-        }
+    override fun onPause() {
+        super.onPause()
+
+        binding.mapView?.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        binding.mapView?.onStop()
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        binding.mapView?.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        binding.mapView?.onSaveInstanceState(outState)
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+
+        binding.mapView?.onLowMemory()
     }
 
     companion object {
--- EarthquakeList/src/main/java/com/jsbomb/earthquakelist/ui/EarthquakeDetailsViewModel.kt
+++ EarthquakeList/src/main/java/com/jsbomb/earthquakelist/ui/EarthquakeDetailsViewModel.kt
@@ -1,21 +1,15 @@
 package com.jsbomb.earthquakelist.ui
 
-import android.net.Uri
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import java.util.*
 
 class EarthquakeDetailsViewModel : ViewModel() {
 
     private val _earthquakeDetails = MutableLiveData<EarthquakeDetails?>(null)
     val earthquakeDetails: LiveData<EarthquakeDetails?>
         get() = _earthquakeDetails
-
-    private val _queryString = MutableLiveData<String?>(null)
-    val queryString: LiveData<String?>
-        get() = _queryString
 
     fun setEarthquakeDetails(
         time: Long = 0,
@@ -36,34 +30,6 @@ class EarthquakeDetailsViewModel : ViewModel() {
             )
         )
     }
-
-    fun onQueryLocation() {
-        _earthquakeDetails.value?.let {
-            // marker but zoomed in too close
-            val format = "geo:%s,%s?z=10&q=%s"
-            val queryFormat = "%s,%s(%s M%.1f) at %s"
-
-            val encodedQuery: String = Uri.encode(
-                String.format(
-                    Locale.US,
-                    queryFormat,
-                    it.latitude,
-                    it.longitude,
-                    it.place,
-                    it.mag,
-                    it.time.formatTime()
-                )
-            )
-            val uriString: String =
-                String.format(Locale.US, format, it.latitude, it.longitude, encodedQuery)
-
-            _queryString.postValue(uriString)
-        }
-    }
-
-    fun onQueryLocationStart() {
-        _queryString.postValue(null)
-    }
 }
 
 data class EarthquakeDetails internal constructor(
@@ -71,8 +37,8 @@ data class EarthquakeDetails internal constructor(
     var place: String? = null,
     var mag: Double = 0.0,
     var longitude: Double = 0.0,
-    val latitude: Double = 0.0,
-    val tsunami: Int = 0
+    var latitude: Double = 0.0,
+    var tsunami: Int = 0
 )
 
 class EarthquakeDetailsViewModelFactory : ViewModelProvider.Factory {
